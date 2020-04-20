9503cd914b53d7ab1da62cd0509f78298e5a76ef
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-04-20 15:37:23 -0400

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Angela Lim on 4/20/20.
+//  Copyright © 2020 Angela Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
